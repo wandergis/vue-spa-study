00b8024a8526dc96ca214b506e7536c5316dce52
wandergis <763393390@qq.com>
2015-12-09 12:58:36 +0800

    add html-webpack-plugin&add html template

---
+++ src/components/about.vue
@@ -0,0 +1,22 @@
+/**
+* Created by WangMing on 15/12/9.
+*/
+<style>
+
+</style>
+
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                msg:"这是关于的页面"
+            }
+        }
+    }
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,18 @@
+/**
+* Created by WangMing on 15/12/9.
+*/
+<style>
+
+</style>
+
+<template>
+
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {}
+        }
+    }
+</script>
